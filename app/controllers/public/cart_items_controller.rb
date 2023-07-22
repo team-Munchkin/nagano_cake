a0bc64c15825ee9c1185657761c1c9705139deb7
ogi0116 <135587780+ogi0116@users.noreply.github.com>
2023-07-22 00:51:33 +0000

    カート内商品の完成

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -1,11 +1,46 @@
 class Public::CartItemsController < ApplicationController
+
+before_action :authenticate_customer!
+
   def index
+    @cart_item = CartItem.all
   end
-  
+
   def create
-    @cart_item = current_user.cart_items.build(cart_item_params)
-    @cart_items = current_user.cart_items.all
+    cart_item = CartItem.new(cart_item_params)
+    cart_item.customer_id = current_customer.id
+    cart_item.item_id = cart_item_params[:item_id]
+    if CartItem.find_by(item_id: params[:cart_item][:item_id]).present?
+      cart_item = CartItem.find_by(item_id: params[:cart_item][:item_id])
+      cart_item.quantity += params[:cart_item][:quantity].to_i
+      cart_item.update(quantity: cart_item.quantity)
+      redirect_to cart_items_path
+    else
+      cart_item.save
+      redirect_to cart_items_path
+    end
   end
-  
-  
+
+  def update
+    cart_item = CartItem.find(params[:id])
+    cart_item.update(cart_item_params)
+    redirect_to cart_items_path
+  end
+
+  def destroy
+    cart_items = CartItem.find_by(id: params[:id],customer_id: current_customer.id)
+    cart_items.destroy
+    redirect_to cart_items_path
+  end
+
+  def destroy_all
+    current_customer.cart_items.destroy_all
+    redirect_to cart_items_path
+  end
+
+  private
+  def cart_item_params
+    params.require(:cart_item).permit(:item_id, :quantity, :id, :customer_id)
+  end
+
 end
--- app/models/customer.rb
+++ app/models/customer.rb
@@ -3,8 +3,9 @@ class Customer < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-         
+
   has_many :cart_items, dependent: :destroy
+  has_many :items, through: :cart_items
 
   validates :family_name,length: { in: 1..10 }
   validates :first_name,length: { in: 1..10 }
